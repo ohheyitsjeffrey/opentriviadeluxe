c1ca430cbf6a2e0523bb61f88587caee36d5b49c
Dave Benvenuti <davebenvenuti@gmail.com>
2022-01-08 15:15:27 -0800

    Create the db on Rails app startup if it doesn't already exist and automatically migrate on startup

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -4,5 +4,12 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /trivia/tmp/pids/server.pid
 
+bin/rake db:create
+bin/rake db:migrate
+
+if [[ "${RAILS_ENV}" == "development" ]]; then
+  bin/rake db:seed
+fi
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
