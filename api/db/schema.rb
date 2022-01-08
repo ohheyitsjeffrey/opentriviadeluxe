95b73b00039ebd060b499116bd81043d7599a313
Dave Benvenuti <davebenvenuti@gmail.com>
2022-01-08 15:16:44 -0800

    Create the db on Rails app startup if it doesn't already exist and automatically migrate on startup (#7)

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,17 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_104_041_012) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2022_01_04_041012) do
 
-  create_table 'games', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'join_code', null: false
-    t.integer 'status', default: 0, null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['join_code'], name: 'index_games_on_join_code'
-    t.index ['status'], name: 'index_games_on_status'
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "join_code", null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["join_code"], name: "index_games_on_join_code"
+    t.index ["status"], name: "index_games_on_status"
   end
+
 end
