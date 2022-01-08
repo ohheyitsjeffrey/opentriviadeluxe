853323a7a59d851ea287691c4c9bcf89b03ff2a0
jeffrey <jeffbenvenuti@gmail.com>
2022-01-08 15:54:47 -0800

    init create react app with typescript

---
+++ ui/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Trivia App</h1>
+      <img src={"logo.gif"} alt="logo" />
+      <p>
+        Coming Soon.
+      </p>
+    </div>
+  );
+}
+
+export default App;
