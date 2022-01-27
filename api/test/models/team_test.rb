3a6f99926f69678f4824d8165b66a841c09bbb69
Dave Benvenuti <davebenvenuti@gmail.com>
2022-01-26 20:00:40 -0800

    Define Team model

---
+++ api/test/models/team_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class TeamTest < ActiveSupport::TestCase
+  test 'requires name to be present' do
+    team = build(:team, name: nil)
+
+    assert_not team.valid?
+
+    team.name = ''
+
+    assert_not team.valid?
+
+    team.name = 'some name'
+
+    assert team.valid?
+  end
+
+  test 'belongs to a game' do
+    team = create(:team)
+
+    assert team.game.present?
+    assert team.game.is_a?(Game)
+  end
+end
