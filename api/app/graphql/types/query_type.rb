7f19759805158ae233358b831e5995e775f1fee5
Dave Benvenuti <davebenvenuti@gmail.com>
2022-03-20 16:52:27 -0700

    Adds a gameForJoinCode query

--- api/app/graphql/types/query_type.rb
+++ api/app/graphql/types/query_type.rb
@@ -2,7 +2,12 @@
 
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :game_for_join_code, Types::GameType do
+      argument :join_code, String, required: true
+    end
+
+    def game_for_join_code(join_code:)
+      Game.find_by!(join_code: join_code)
+    end
   end
 end
